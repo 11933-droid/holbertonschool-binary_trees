9a47d23adf423425cffc76dc63016e41ba861dc0
Yasir Musaad Alshabanah <Yaser.al.shabanah@gmail.com>
2025-10-26 20:44:54 +0000

    feat(task13): binary_tree_nodes() - count nodes with children

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts nodes with at least 1 child
+ * @tree: pointer to the root node
+ *
+ * Return: count of non-leaf nodes, or 0 if tree is NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+if (!tree)
+return (0);
+
+if (!tree->left && !tree->right)
+return (0);
+
+return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
+
